a0ef8f1f5446b27987b38a0a8c57bd2523904384
HawkWolf1 <singhavinash9fg@gmail.com>
2023-06-29 16:00:12 +0530

    working

---
+++ src/components/counter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Counter = () => {
+  return (
+    <div>
+      <h1>Counter</h1>
+      {/* counter component code  */}
+    </div>
+  );
+};
+
+export default Counter;
